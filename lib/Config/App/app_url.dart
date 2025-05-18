21b5c455c79d4337ab6d337737809670356eafbd
Masab Haider <masabhaider4@gmail.com>
2025-05-19 02:30:21 +0500

    feat: Update permissions and add permission_handler dependency

--- lib/Config/App/app_url.dart
+++ lib/Config/App/app_url.dart
@@ -2,9 +2,9 @@ class AppUrl {
   // Base Url For IOS
   // static const String baseUrl = 'http://localhost:3000/';
   // Local Base Url For Android Emulator
-  static const String baseUrl = 'http://192.168.0.177:3000/';
+  // static const String baseUrl = 'http://192.168.0.177:3000/';
   // Production Url
-  // static const String baseUrl = 'https://zap-docs-backend.vercel.app/';
+  static const String baseUrl = 'https://zap-docs-backend.vercel.app/';
   // Login
   static const String login = '${baseUrl}user/login';
   // Register
